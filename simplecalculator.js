4ba0578488fe39643bcf64729fde10754c346f75
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-19 21:48:03 +0500

    logic function that read values for the calculator app

--- simplecalculator.js
+++ simplecalculator.js
@@ -1,9 +1,19 @@
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
+const num1Input = document.getElementById('num1');
+const num2Input = document.getElementById('num2');
 
-const addBtn= document.getElementById('addBtn');
+const  addBtn= document.getElementById('addBtn');
 const subBtn = document.getElementById('subBtn');
 const mulBtn = document.getElementById('mulBtn');
 const divBtn = document.getElementById('divBtn');
 
-const result = document.getElementById('result');
+function getNumbers(){
+    const num1 = parseFloat(num1Input.value);
+    const num2 = parseFloat(num2Input.value);
+    if (isNaN(num1) || isNaN(num2)){
+    
+        alert('Please enter a valid number.')
+        return null;
+    }
+    return{num1, num2}
+    
+    }
