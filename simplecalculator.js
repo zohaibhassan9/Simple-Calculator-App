b486a1d88259376194789d9d42cf21b5401ff2ad
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-19 20:40:34 +0500

    all id feteched through getElementById's method

---
+++ simplecalculator.js
@@ -0,0 +1,9 @@
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+
+const addBtn= document.getElementById('addBtn');
+const subBtn = document.getElementById('subBtn');
+const mulBtn = document.getElementById('mulBtn');
+const divBtn = document.getElementById('divBtn');
+
+const result = document.getElementById('result');
