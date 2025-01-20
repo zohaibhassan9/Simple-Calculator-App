e42ea00a1f56281169815310bcc41a5ee7e5d971
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-20 09:25:41 +0500

    we have completed this 'Simple Calculator App' by 2nd method also .

--- simplecalculator.js
+++ simplecalculator.js
@@ -30,45 +30,88 @@ function getNumbers(){
 // (i) The first one is given below
 
 
-function getResults(operation){
+// function getResults(operation){
+// const numbers = getNumbers();
+
+// if(!numbers) return;
+
+// const {num1, num2} = numbers;
+
+// let result;
+
+// switch(operation){
+
+//     case 'add':
+//     result = num1 + num2;
+//     break;
+//     case 'subtract':
+//     result = num1 - num2;
+//     break;
+//     case 'multiply':
+//     result = num1*num2;
+//     break
+//     case "division":
+//     result = num2 !== 0 ? num1/num2: 'Division by 0 is not allowed.';
+//     break;
+//     default:
+//     alert('Invalid operation')
+//     return;
+
+// }
+
+// resultDisplay.textContent = result;
+// }
+
+// addBtn.addEventListener('click', () => getResults('add'));
+// subBtn.addEventListener('click', () => getResults('subtract'));
+// mulBtn.addEventListener('click', () => getResults('multiply'));
+// divBtn.addEventListener('click', () => getResults('division'));
+
+
+
+// ii) The Second method for the simple calculator are given below 
+// for addition
+addBtn.addEventListener('click', ()=>{
 const numbers = getNumbers();
 
-if(!numbers) return;
-
-const {num1, num2} = numbers;
-
-let result;
-
-switch(operation){
-
-    case 'add':
-    result = num1 + num2;
-    break;
-    case 'subtract':
-    result = num1 - num2;
-    break;
-    case 'multiply':
-    result = num1*num2;
-    break
-    case "division":
-    result = num2 !== 0 ? num1/num2: 'Division by 0 is not allowed.';
-    break;
-    default:
-    alert('Invalid operation')
-    return;
-
+if(numbers){
+    const {num1, num2} = numbers;
+    resultDisplay.textContent = `Result: ${num1 + num2}`;
 }
 
-resultDisplay.textContent = result;
+})
+// for subtraction
+subBtn.addEventListener('click', ()=>{
+const numbers = getNumbers();
+
+if(numbers){
+    const {num1, num2} = numbers;
+    resultDisplay.textContent = `Result: ${num1 - num2}`;
 }
+})
 
-addBtn.addEventListener('click', () => getResults('add'));
-subBtn.addEventListener('click', () => getResults('subtract'));
-mulBtn.addEventListener('click', () => getResults('multiply'));
-divBtn.addEventListener('click', () => getResults('division'));
+// for multiplication
+mulBtn.addEventListener('click', ()=>{
+    const numbers = getNumbers();
+    
+    if(numbers){
+        const {num1, num2} = numbers;
+        resultDisplay.textContent = `Result: ${num1 * num2}`
+    }
+    })
 
+    // for division
+    divBtn.addEventListener('click', ()=>{
+        const numbers = getNumbers();
+        
+        if(numbers){
+            const {num1, num2} = numbers;
+            resultDisplay.textContent = `Result: ${num1 / num2}`
 
-
-// The Second method for the simple calculator are given below 
-
-
+            if(num2 === 0){
+                alert('Please divide bt 0 are not allowed')
+                return;
+            }
+            
+        }
+        })
